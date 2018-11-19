2d1173f4d75fb06fcf4ed7fe37eb7936e0917ca6
Garrett Cullen <garrettwcullen@gmail.com>
2018-11-19 11:12:16 -0800

    push

--- wp-content/themes/fang/page-templates/homepage_template_parts/section-2.php
+++ wp-content/themes/fang/page-templates/homepage_template_parts/section-2.php
@@ -1,13 +1,35 @@
 <section id="section_two">
 	
-<!-- 	<img class="sec_two_bg" src="<?php bloginfo('template_directory');?>/images/hero_img_background_desktop.jpg"/> -->
-	
 	<div class="sec_two_inner">
 		
-		
+		<div class="sec_two_content content">
+			
+			<h1>Colorado Personal Injury Attorneys</h1>
+			
+			<div class="sec_two_content_svg">
+				
+				<?php echo file_get_contents("wp-content/themes/fang/images/content02_icon-02.svg"); ?>
+				
+			</div><!-- sec_two_content_svg -->
+			
+			<div class="sec_two_inner_content">
+			
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua dolor sed ut unde omnis iste cilllum duis aute coluptate.</p>
+
+				<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste </p>
+			
+			</div><!-- sec_two_inner_content -->
+			
+		</div><!-- sec_two_content -->
 		
 	</div><!-- sec_two_inner -->
 	
 	<div class="sec_two_bg"></div>
+	
+	<div class="sec_two_topog">
+		
+		<?php echo file_get_contents("wp-content/themes/fang/images/topog.svg"); ?>
+		
+	</div><!-- sec_two_topog -->
 	
 </section><!-- section_two -->
