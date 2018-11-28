4d20cd47e0b4d414f7b8e633071c473a4dfeb2be
Garrett Cullen <garrettwcullen@gmail.com>
2018-11-28 09:35:16 -0800

    push

--- wp-content/themes/fang/page.php
+++ wp-content/themes/fang/page.php
@@ -1,12 +1,43 @@
 <?php get_header(); ?>
 
+<div id="internal_trigger" class="internal_main">
+	
+	
+	<div class="internal_banner">
+		
+		<div class="internal_banner_inner">
+		
+			<img class="internal_bg" src="<?php bloginfo('template_directory');?>/images/intl_blog_header_img_01.jpg"/><!-- internal_bg -->
+		
+			<img class="internal_img_one" src="<?php bloginfo('template_directory');?>/images/intl_blog_header_img_02.jpg"/><!-- internal_img_one -->
+			
+			<img class="internal_img_two" src="<?php bloginfo('template_directory');?>/images/intl_blog_header_img_03.jpg"/><!-- internal_img_two -->
+			
+			<img class="internal_img_three" src="<?php bloginfo('template_directory');?>/images/intl_blog_header_img_04.jpg"/><!-- internal_img_two -->
+		
+		</div><!-- internal_banner_inner -->
+		
+	</div><!-- internal_banner -->
+	
+	<div class="container two_col">
+		
+		<?php get_sidebar(); ?>
+		
+		<div class="content_wrapper content">
+			
+			
+		</div><!-- content_wrapper -->
+		
+	</div><!-- container -->
+	
+</div><!-- internal_main -->
 
 		
-	<?php get_template_part( 'loop', 'page' ); ?>
+	<?php // get_template_part( 'loop', 'page' ); ?>
 		
 	
 	
-	<?php get_sidebar(); ?>
+	
 	
 
 
--- wp-content/themes/fang/sidebar.php
+++ wp-content/themes/fang/sidebar.php
@@ -1,1 +1,21 @@
+<div class="sidebar_wrapper">
+	
+	<div class="sidebar_form">
+		
+		<div class="sidebar_form_inner">
+			
+			<span class="small_header">Please fill out the form below for your</span><!-- small_header -->
+			
+			<span class="large_header">Free Consultation</span><!-- large_header -->
+			
+			<img class="sidebar_accent" src="<?php bloginfo('template_directory');?>/images/content03_icon-01.svg"/>
+			
+		</div><!-- sidebar_form_inner -->
+		
+	</div><!-- sidebar_form -->
+	
+</div><!-- sidebar_wrapper -->
+	
+	
+	
 	<?php // wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
