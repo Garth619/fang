1977adf9fafb110fcad5f3f24b64417c16777815
Garrett Cullen <garrettwcullen@gmail.com>
2018-11-29 15:43:15 -0800

    push

--- wp-content/themes/fang/page-templates/template-testimonials.php
+++ wp-content/themes/fang/page-templates/template-testimonials.php
@@ -5,6 +5,77 @@
 get_header(); ?>
 
 
+<div id="internal_trigger" class="internal_main">
+	
+	<div class="grey_topog"><?php echo file_get_contents("wp-content/themes/fang/images/topog.svg"); ?></div><!-- grey_topog -->
+	
+	<div class="container one_col">
+		
+		<h1 class="internal_title"><?php the_title();?></h1><!-- internal_title -->
+		
+		<div class="testimonial_wrapper">
+		
+			<div class="single_test">
+				
+				<img class="star" src="<?php bloginfo('template_directory');?>/images/test_icon-01.svg"/>
+				
+				<span class="test_title">Remington Fang is a very knowledgeable trustworthy Attorney.</span><!-- test_title -->
+				
+				<span class="test_description">“He has helped me in so many ways, gave me wonderful advice, calmed my nerves, and walked me through my options. I highly recommend him!! Even after my case was handed back to me he still answered any question I had and answered them with no regard to himself. Thank you so much for everything Remington."</span><!-- test_description -->
+				
+				<img class="test_accent" src="<?php bloginfo('template_directory');?>/images/six_count.svg"/>
+				
+				<span class="test_name">Shyanne</span><!-- test_name -->
+				
+			</div><!-- single_test -->
+			
+			<div class="single_test">
+				
+				<img class="star" src="<?php bloginfo('template_directory');?>/images/test_icon-01.svg"/>
+				
+				<span class="test_title">Remington Fang is a very knowledgeable trustworthy Attorney.</span><!-- test_title -->
+				
+				<span class="test_description">“He has helped me in so many ways, gave me wonderful advice, calmed my nerves, and walked me through my options. I highly recommend him!! Even after my case was handed back to me he still answered any question I had and answered them with no regard to himself. Thank you so much for everything Remington."</span><!-- test_description -->
+				
+				<img class="test_accent" src="<?php bloginfo('template_directory');?>/images/six_count.svg"/>
+				
+				<span class="test_name">Shyanne</span><!-- test_name -->
+				
+			</div><!-- single_test -->
+			
+			<div class="single_test">
+				
+				<img class="star" src="<?php bloginfo('template_directory');?>/images/test_icon-01.svg"/>
+				
+				<span class="test_title">Remington Fang is a very knowledgeable trustworthy Attorney.</span><!-- test_title -->
+				
+				<span class="test_description">“He has helped me in so many ways, gave me wonderful advice, calmed my nerves, and walked me through my options. I highly recommend him!! Even after my case was handed back to me he still answered any question I had and answered them with no regard to himself. Thank you so much for everything Remington."</span><!-- test_description -->
+				
+				<img class="test_accent" src="<?php bloginfo('template_directory');?>/images/six_count.svg"/>
+				
+				<span class="test_name">Shyanne</span><!-- test_name -->
+				
+			</div><!-- single_test -->
+			
+			<div class="single_test">
+				
+				<img class="star" src="<?php bloginfo('template_directory');?>/images/test_icon-01.svg"/>
+				
+				<span class="test_title">Remington Fang is a very knowledgeable trustworthy Attorney.</span><!-- test_title -->
+				
+				<span class="test_description">“He has helped me in so many ways, gave me wonderful advice, calmed my nerves, and walked me through my options. I highly recommend him!! Even after my case was handed back to me he still answered any question I had and answered them with no regard to himself. Thank you so much for everything Remington."</span><!-- test_description -->
+				
+				<img class="test_accent" src="<?php bloginfo('template_directory');?>/images/six_count.svg"/>
+				
+				<span class="test_name">Shyanne</span><!-- test_name -->
+				
+			</div><!-- single_test -->
+		
+		</div><!-- testimonial_wrapper -->
+				
+	</div><!-- container -->
+	
+</div><!-- internal_main -->
 
 
 <?php get_footer(); ?>
