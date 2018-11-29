f8e6b83df37e433d602ff9a080dcb677ded02f29
Garrett Cullen <garrettwcullen@gmail.com>
2018-11-29 10:19:58 -0800

    push

--- wp-content/themes/fang/page.php
+++ wp-content/themes/fang/page.php
@@ -26,37 +26,10 @@
 		<div class="content_wrapper content">
 			
 			
-			<h1 class="page_title">H1 Line one goes here and there goes another</h1><!-- page_title -->
+			<h1 class="page_title"><?php the_title();?></h1><!-- page_title -->
 			
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-			<h2>H2 lorem ipsum dolor sit amet, consecter adipisin elit sed do eiusmod tempor</h2>	
-
-			<p>Body copy enim ad minim veniam, <a href="">embeded link</a> exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus</p>
-
-
-			<h3>H3 emo enim ipsam voluptatem quia voluptas sit aspernat</h3> 
-			
-			<p>sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur</p>
-			
-			<blockquote>Pull quote style ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et</blockquote>
-			
-			
-			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit:</p>
-
-			<ul>
-				<li>Bulleted list item number one</li>
-				<li>Bulleted list item number two</li>
-				<li>Bulleted list item number three</li>
-			</ul>
-			<ol>
-				<li>Number list item number one</li>
-				<li>Number list item number two</li>
-				<li> Number list item number three</li>
-			</ol>
-
- 
-
+			 
+			<?php get_template_part( 'loop', 'page' ); ?>
 			
 			
 		</div><!-- content_wrapper -->
@@ -64,15 +37,6 @@
 	</div><!-- container -->
 	
 </div><!-- internal_main -->
-
-		
-	<?php // get_template_part( 'loop', 'page' ); ?>
-		
-	
-	
-	
-	
-
 
 
 <?php get_footer(); ?>
