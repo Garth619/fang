2c96a87e11d5056441bf8c0d2440ffd35ed9f4b8
Garrett Cullen <garrettwcullen@gmail.com>
2018-11-30 10:58:51 -0800

    push

--- wp-content/themes/fang/page-templates/template-video.php
+++ wp-content/themes/fang/page-templates/template-video.php
@@ -15,131 +15,39 @@ get_header(); ?>
 		
 		<div class="video_wrapper">
 			
-			<div class="single_video">
-				
-				<div class="wistia_wrapper">
-					
-					<div class="wistia_inner wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail"></div><!-- wistia_inner -->
-					
-					<div class="wistia_overlay_intial"></div><!-- wistia_overlay_intial -->
-					
-					<div class="wistia_overlay">
-						
-						<div class="play_button_internal">
-							
-							<div class="play_button_inner"></div><!-- play_button_inner -->
-							
-						</div><!-- play_button_internal -->
-						
-					</div><!-- wistia_overlay -->
-					
-				</div><!-- wistia_wrapper -->
-				
-				
-				<span class="video_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">Lorem ipsum dolor emo enim ipsam voluptatem quia voluptas sit aspernat</span><!-- video_title -->
-				
-			</div><!-- single_video -->
 			
-			<div class="single_video">
+			<?php if(get_field('videos')): ?>
+			 
+				<?php while(has_sub_field('videos')): ?>
+			 
+					<div class="single_video">
 				
-				<div class="wistia_wrapper">
+						<div class="wistia_wrapper">
 					
-					<div class="wistia_inner wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail"></div><!-- wistia_inner -->
+							<div class="wistia_inner wistia_embed wistia_async_<?php the_sub_field( 'wistia_id' ); ?> popover=true popoverContent=thumbnail"></div><!-- wistia_inner -->
 					
-					<div class="wistia_overlay_intial"></div><!-- wistia_overlay_intial -->
+								<div class="wistia_overlay_intial"></div><!-- wistia_overlay_intial -->
 					
-					<div class="wistia_overlay">
+								<div class="wistia_overlay">
 						
-						<div class="play_button_internal">
+									<div class="play_button_internal">
 							
-							<div class="play_button_inner"></div><!-- play_button_inner -->
+										<div class="play_button_inner"></div><!-- play_button_inner -->
 							
-						</div><!-- play_button_internal -->
+									</div><!-- play_button_internal -->
 						
-					</div><!-- wistia_overlay -->
+								</div><!-- wistia_overlay -->
 					
-				</div><!-- wistia_wrapper -->
+							</div><!-- wistia_wrapper -->
 				
+						<span class="video_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html"><?php the_sub_field( 'video_title' ); ?></span><!-- video_title -->
 				
-				<span class="video_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">Lorem ipsum dolor emo enim ipsam voluptatem quia voluptas sit aspernat</span><!-- video_title -->
-				
-			</div><!-- single_video -->
+				</div><!-- single_video -->
+			    
+				<?php endwhile; ?>
+			 
+			<?php endif; ?>
 			
-			<div class="single_video">
-				
-				<div class="wistia_wrapper">
-					
-					<div class="wistia_inner wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail"></div><!-- wistia_inner -->
-					
-					<div class="wistia_overlay_intial"></div><!-- wistia_overlay_intial -->
-					
-					<div class="wistia_overlay">
-						
-						<div class="play_button_internal">
-							
-							<div class="play_button_inner"></div><!-- play_button_inner -->
-							
-						</div><!-- play_button_internal -->
-						
-					</div><!-- wistia_overlay -->
-					
-				</div><!-- wistia_wrapper -->
-				
-				
-				<span class="video_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">Lorem ipsum dolor emo enim ipsam voluptatem quia voluptas sit aspernat</span><!-- video_title -->
-				
-			</div><!-- single_video -->
-			
-			<div class="single_video">
-				
-				<div class="wistia_wrapper">
-					
-					<div class="wistia_inner wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail"></div><!-- wistia_inner -->
-					
-					<div class="wistia_overlay_intial"></div><!-- wistia_overlay_intial -->
-					
-					<div class="wistia_overlay">
-						
-						<div class="play_button_internal">
-							
-							<div class="play_button_inner"></div><!-- play_button_inner -->
-							
-						</div><!-- play_button_internal -->
-						
-					</div><!-- wistia_overlay -->
-					
-				</div><!-- wistia_wrapper -->
-				
-				
-				<span class="video_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">Lorem ipsum dolor emo enim ipsam voluptatem quia voluptas sit aspernat</span><!-- video_title -->
-				
-			</div><!-- single_video -->
-			
-			<div class="single_video">
-				
-				<div class="wistia_wrapper">
-					
-					<div class="wistia_inner wistia_embed wistia_async_waichudc62 popover=true popoverContent=thumbnail"></div><!-- wistia_inner -->
-					
-					<div class="wistia_overlay_intial"></div><!-- wistia_overlay_intial -->
-					
-					<div class="wistia_overlay">
-						
-						<div class="play_button_internal">
-							
-							<div class="play_button_inner"></div><!-- play_button_inner -->
-							
-						</div><!-- play_button_internal -->
-						
-					</div><!-- wistia_overlay -->
-					
-				</div><!-- wistia_wrapper -->
-				
-				
-				<span class="video_title wistia_embed wistia_async_waichudc62 popover=true popoverContent=html">Lorem ipsum dolor emo enim ipsam voluptatem quia voluptas sit aspernat</span><!-- video_title -->
-				
-			</div><!-- single_video -->
-						
 		</div><!-- video_wrapper -->
 				
 	</div><!-- container -->
