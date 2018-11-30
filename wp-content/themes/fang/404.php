811610fc0fbb8f66cd5cc9dd957e467828111cfe
Garrett Cullen <garrettwcullen@gmail.com>
2018-11-30 15:24:16 -0800

    push

--- wp-content/themes/fang/404.php
+++ wp-content/themes/fang/404.php
@@ -1,9 +1,30 @@
 <?php get_header(); ?>
 
 
-<span onclick="goBack()">go back</span>
+
+<div id="internal_trigger" class="internal_main">
 	
-<!--
+	<div class="grey_topog"><?php echo file_get_contents("wp-content/themes/fang/images/topog.svg"); ?></div><!-- grey_topog -->
+	
+	<div class="container one_col">
+		
+		<div class="not_found_wrapper">
+			
+			<h1 class="not_found">Page Not Found</h1><!-- not_found -->
+			
+			<img src="<?php bloginfo('template_directory');?>/images/content01_icon-01.svg"/>
+			
+			<span class="error_code">Error Code: 404</span><!-- error_code -->
+			
+			<span class="not_found_description">The page you were looking for appears to have been moved, deleted or does not exist. You could <span class="go_back" onclick="goBack()">go back</span> to where you were or head straight to our <span class="yellow">home page</span>. </span><!-- not_found_description -->
+							
+		</div><!-- not_found_wrapper -->
+				
+	</div><!-- container -->
+	
+</div><!-- internal_main -->
+				
+
 	<script type="text/javascript">
 
 		function goBack() {
@@ -11,7 +32,7 @@
 		}
 
 	</script>
--->
+
 	
 
 <?php get_footer(); ?>
--- wp-content/themes/fang/page-templates/template-caseresults.php
+++ wp-content/themes/fang/page-templates/template-caseresults.php
@@ -5,7 +5,48 @@
 get_header(); ?>
 
 
-						
+<div id="internal_trigger" class="internal_main">
+	
+	<div class="grey_topog"><?php echo file_get_contents("wp-content/themes/fang/images/topog.svg"); ?></div><!-- grey_topog -->
+	
+	<div class="container one_col">
+		
+		<h1 class="internal_title"><?php the_title();?></h1><!-- internal_title -->
+		
+		<div class="case_results_internal">
+			
+			
+			<?php if(get_field('case_results_internal')): ?>
+			 
+				<?php while(has_sub_field('case_results_internal')): ?>
+				
+				
+					<div class="single_cr">
+				
+						<img src="<?php bloginfo('template_directory');?>/images/content02_icon-01.svg"/>
+				
+						<span class="cr_amount"><?php the_sub_field( 'case_results_amount' ); ?></span><!-- cr_amount -->
+				
+						<span class="cr_type"><?php the_sub_field( 'case_results_type_internal' ); ?></span><!-- cr_type -->
+				
+						<span class="cr_description"><?php the_sub_field( 'case_results_description' ); ?></span><!-- cr_description -->
+				
+						<img class="cr_six_count" src="<?php bloginfo('template_directory');?>/images/six_count.svg"/>
+				
+					</div><!-- single_cr -->
+			 
+				
+				<?php endwhile; ?>
+			 
+			<?php endif; ?>
+			
+				
+		</div><!-- case_results_internal -->
+				
+	</div><!-- container -->
+	
+</div><!-- internal_main -->
+				
 			  
 
 
