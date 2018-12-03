78757e5545878af9861155e64401436ee199818a
Garrett Cullen <Garth619@users.noreply.github.com>
2018-12-02 22:12:06 -0800

    push

--- wp-content/themes/fang/page-templates/template-bio.php
+++ wp-content/themes/fang/page-templates/template-bio.php
@@ -5,8 +5,42 @@
 get_header(); ?>
 
 
+<div id="internal_trigger" class="internal_main">
+	
+	<div class="container att_col content">
+		
+		<div class="att_bio_left">
+			
+			<img src="<?php bloginfo('template_directory');?>/images/ap_img_remington.jpg"/>
+			
+			<blockquote>Pull quote style ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </blockquote>
+			
+		</div><!-- att_bio_left -->
+		
+		<div class="att_bio_right">
+		
+			<h1 class="internal_title"><?php the_title();?></h1><!-- internal_title -->
+			
+			<h2>From a young age, Remington has always despised bullies.</h2>
+			
+			<p>Today, as a personal injury attorney fighting for the rights of injured individuals against corporations that value profit over safety, Remington has the opportunity to tackle bullies all day long.  A native of Colorado Springs, Colorado, Remington grew up in a family that valued hard-work, decency, and a respect for the rights of every individual. Remington’s mother is a U.S. Army veteran who now specializes in Ethnological Studies.  Remington’s father is an Oriental Medical Doctor and holistic healer who was the second licensed acupuncturist in the state of Colorado. Remington's step-father is an astrophysicist and cosmologist.  From the CEO to the janitor, Remington believes that the human spirit and the power of each person to make a positive difference in this world should not be abated or diminished by an injury or loss- especially due to the carelessness or negligence of someone else.</p>
 
-	<?php get_template_part( 'loop', 'page' ); ?>
+			<p>After attending Saint Mary’s Catholic High School in Colorado Springs, Remington went on to obtain his B.A. from the University of Northern Colorado where he walked on to the UNC Football Team through the Grey Shirt Program, was an active member of the Student Representative Council, and had a little fun in the meantime.   After graduating from UNC, Remington attended the University of Arkansas School of Law on a scholarship.  While at Arkansas, Remington worked for the law firm of Mays, Byrd and Associates in Little Rock, Walmart International in Bentonville, and was research assistant to Professor Timothy Tarvin, whose groundbreaking research into bankruptcy law has helped many disadvantaged people.</p>
+
+			<p>After law school, Remington began working as a personal injury attorney for some of the largest law firms in the state, handling thousands of cases from pre-litigation settlement negotiations to jury trials and appeals.  Remington has litigated cases in nearly every county on the front range and beyond.  To date, Remington has recovered tens of millions of dollars for his clients to pay for their medical expenses, lost wages, and suffering, but he is not done yet.  Remington, as a member of the Board of Directors for the Colorado Trial Lawyers Association, envisions a world where the law helps the people.  That is why Remington is a frequent lecturer and vocal advocate for just laws that protect individuals.</p>
+
+			<p>If you have been injured, and need a down-to-earth attorney who understands and fights for your rights, call The Fang Law Firm today.  We do not judge, we only help.</p>
+
+
+		
+		</div><!-- att_bio_right -->
+		
+		
+						
+	</div><!-- container -->
+	
+</div><!-- internal_main -->
+
 					 
 					 	
 <?php get_footer(); ?>
