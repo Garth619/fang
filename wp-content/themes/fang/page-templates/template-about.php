fe35db91e05155a445ebf4fa6de601d20c20b3b9
Garrett Cullen <Garth619@users.noreply.github.com>
2018-12-04 19:42:57 -0800

    push

--- wp-content/themes/fang/page-templates/template-about.php
+++ wp-content/themes/fang/page-templates/template-about.php
@@ -17,9 +17,9 @@ get_header(); ?>
 			
 				<div class="about_banner_content">
 				
-					<span>Best Personal Injury Attorneys in Denver</span>
+					<span><?php the_field( 'small_header' ); ?></span>
 				
-					<h1>About Our Firm</h1>
+					<h1><?php the_field( 'header' ); ?></h1>
 				
 					<a class="button desktop" href="#consultation">Click For a Free Consultation</a>
 			
@@ -49,9 +49,9 @@ get_header(); ?>
 			
 			<img src="<?php bloginfo('template_directory');?>/images/content02_icon-01.svg"/>
 			
-			<blockquote>The Fang Law Firm took my case when others looked past me and believed that I was not worth their time.</blockquote>
+			<blockquote><?php the_field( 'about_blockquote' ); ?></blockquote>
 			
-			<span class="blockquote_name">Jacob From Boulder</span><!-- blockquote_name -->
+			<span class="blockquote_name"><?php the_field( 'about_blockquote_name' ); ?></span><!-- blockquote_name -->
 			
 		</div><!-- blockquote_wrapper -->
 		
@@ -65,15 +65,8 @@ get_header(); ?>
 			
 			<div class="about_right">
 				
-				<h2>We Are Not The TV Guys</h2>
-				
-				<p>The Fang Law Firm was founded on one guiding principle: People deserve personal representation and communication throughout their personal injury claim.  The founders of the firm spent years in large television advertising law firms where each attorney handled hundreds of cases at any given time.  While we were able to help many people, we realized that it is impossible for one attorney, even a talented one, to handle so many cases.  Attorneys should have enough time to speak with their clients regularly, be in contact with doctors, update insurance companies, and handle financial matters on behalf of their clients.  This is the level of service we promise at The Fang Law Firm.</p>
-
-
-				<h2>Attorneys Should Run Your Case – Not Paralegals</h2>
-
-				<p>We believe in hands-on case management.  Many law firms will ask paralegals or case managers who have no legal experience to call you and advise you.  With The Fang Law Firm, our attorneys will be your main point of contact and will proactively keep you updated on your case.  We do not wait for our clients to call us.  Our attorneys will take the time to meet with you personally or over the phone to gather every detail of your case, fight hard against the insurance company or responsible party to protect and secure your rights, and ultimately obtain the compensation you deserve for your medical expenses, economic losses, pain and suffering, loss of enjoyment of life, lost wages, and out-of-pocket expenses. </p>
-				
+				<?php the_field( 'about_content_right' ); ?>
+								
 			</div><!-- about_right -->
 			
 		</div><!-- about_content -->
@@ -88,13 +81,7 @@ get_header(); ?>
 			
 			<div class="about_bottom_inner">
 				
-				<h2>Injury Cases Are Complicated and Can Leave Your Finances a Mess</h2>
-				
-				<p>We understand first-hand what it is like to have your life and finances hang in the balance following a collision or other incident that was not your fault.  If you cannot find a doctor, we will find you a doctor who we trust to provide excellent treatment at a reasonable cost.  We will work with the billing departments of hospitals and other medical providers to protect your personal credit.  In Colorado, the insurance company does not need to pay anything until the cases settles- not even your medical expenses.  That is why it is important to hire a lawyer who cares about your case and has enough time to know every inch of your case.  This will allow you to Get Your Life Back On Track.</p>
-
-				<h2>Challenge</h2>
-
-				<p>If you interview a large personal injury law firm, ask the lawyer how many active files they have in litigation and pre-litigation, and ask them how they handle it.  The answer may surprise you.  Most lawyers will say that they have the “staff” to handle it.  Ask yourself - do you want to hire the staff or do you want to hire the attorney?</p>
+				<?php the_field( 'about_content_bottom' ); ?>
 				
 			</div><!-- about_bottom_inner -->
 			
