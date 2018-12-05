2d783b127ddee9b755e30cb11c81d08b3c945011
Garrett Cullen <garrettwcullen@gmail.com>
2018-12-05 11:19:14 -0800

    push

---
+++ wp-content/themes/fang/page-templates/homepage_template_parts/section-4.php
@@ -0,0 +1,37 @@
+<section id="section_four">
+	
+	<div class="sec_four_inner content">
+		
+		
+		<div class="col">
+		
+			<h2>Lorem ipsum dolor sit amet, cons ecter adipisicing elitoe sed do eius mod amet dolore&nbsp;eli</h2>
+		
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquat perspiciatis unde omnis iste natus error sit.</p>
+
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo eiusmod tempor incididunt ut labore et dolore magna.</p>
+
+			<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neq</p>
+		
+		</div><!-- col -->
+		
+		<div class="col">
+		
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquat perspiciatis unde omnis iste natus error&nbsp;sit.</p>
+
+			<h3>Nemo enim ipsam voluptatem quia voluptas sit</h3>
+ 
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		
+			<img class="ram" src="<?php bloginfo('template_directory');?>/images/goat.svg"/>
+		
+			<h3>Nemo enim ipsam voluptatem quia voluptas sit que enim quia aut sed</h3>
+		
+			<p>Sed ut perspiciatis unde omnis iste natus error sit nemo ips voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut</p>
+		
+		</div><!-- col -->
+ 
+		
+	</div><!-- sec_four_inner -->
+	
+</section><!-- section_four -->
