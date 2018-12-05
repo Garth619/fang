9e776f1e7c374e2e3dbf725ad901471c9b6bb0ad
Garrett Cullen <garrettwcullen@gmail.com>
2018-12-05 09:57:05 -0800

    push

--- wp-content/themes/fang/page-templates/homepage_template_parts/section-3.php
+++ wp-content/themes/fang/page-templates/homepage_template_parts/section-3.php
@@ -1,6 +1,35 @@
-<section id="section_three">
+<section id="section_three" class="content">
 	
 	
-	section three
+	<div class="sec_three_blockquote_wrapper">
+		
+		<img src="<?php bloginfo('template_directory');?>/images/content02_icon-01.svg"/>
+		
+		<blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</blockquote>
+		
+	</div><!-- sec_three_blockquote_wrapper -->
+	
+	<div class="sec_three_content">
+		
+		<div class="sec_three_left">
+			
+			<h2>Lorem ipsum dolor sit amet, consecter adipisicing elit sed do eiusmod tempor</h2>
+
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+			
+			<h3>Nemo enim ipsam voluptatem quia voluptas sit aspernat</h3>
+
+			<p>sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non</p>
+			
+			<a class="button desktop" href="#consultation">Click For a Free Consultation</a>
+			
+			<a class="button mobile" href="#consultation">Tap For a Free Consultation</a>
+			
+			</div><!-- sec_three_left -->
+		
+		<div class="sec_three_right"></div><!-- sec_three_right -->
+		
+	</div><!-- sec_three_content -->
+
 	
 </section><!-- section_three -->
